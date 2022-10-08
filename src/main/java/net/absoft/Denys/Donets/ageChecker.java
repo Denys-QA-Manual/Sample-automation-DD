1182f553f0e4c69d01744878d156d3cf6995ff69
Denys Donets <denisdonetswriter@gmail.com>
2022-10-08 15:56:26 +0300

    Commit new branch

--- src/main/java/net/absoft/Denys/Donets/ageChecker.java
+++ src/main/java/net/absoft/Denys/Donets/ageChecker.java
@@ -1,7 +1,7 @@
 package net.absoft.Denys.Donets;
 
 public class ageChecker {
-    private static final int MIN_AGE = 18;
+    private static final int MIN_AGE = 21;
 
     public static boolean canPlayGame(int age) {
         return age >= MIN_AGE;
